4ea2c727d715e25cd849565a3c5014cbb216ae84
Shailesh Rudra <rudra12shailesh@gmail.com>
2016-10-29 23:56:50 +0530

    Function and Class based vew url

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def kirr_redirect_view(request, *args, **kwargs):  # function based view
+    return HttpResponse("HELLO")
+
+
+class KirrCBView(View):  # class based view
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("HELLO Again")
