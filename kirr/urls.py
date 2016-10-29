a6c54ae129ceae8ff2591efb4bd7850ebea86b77
Shailesh Rudra <rudra12shailesh@gmail.com>
2016-10-30 00:24:09 +0530

    URL keyword args

--- kirr/urls.py
+++ kirr/urls.py
@@ -20,6 +20,6 @@ from shortener.views import kirr_redirect_view, KirrCBView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^view-1/$', kirr_redirect_view),
-    url(r'^view-2/$', KirrCBView.as_view()),
+    url(r'^fbv/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
+    url(r'^cbv/(?P<shortcode>[\w-]+)/$', KirrCBView.as_view()),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -2,10 +2,19 @@ from django.http import HttpResponse
 from django.views import View
 
 
-def kirr_redirect_view(request, *args, **kwargs):  # function based view
-    return HttpResponse("HELLO")
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view
+    # print(request.user)
+    # print(request.user.is_authenticated())
+    print(shortcode)
+    print(args)
+    print(kwargs)
+    username = "Guest"
+    if request.user.is_authenticated():
+        username = request.user
+    return HttpResponse("HELLO, {0} \nYour shorcode is {1}".format(username, shortcode))
 
 
 class KirrCBView(View):  # class based view
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("HELLO Again")
+    def get(self, request, shortcode=None, *args, **kwargs):
+        print(shortcode)
+        return kirr_redirect_view(request, shortcode, *args, **kwargs)
